244bc4d3089e62181456c9859f3c404679b028ea
giedriusgruz <34424939+giedriusgruz@users.noreply.github.com>
2017-12-22 15:48:29 +0200

    no message

--- uzduotys/diena10/giedriusgruz/1-js-projektas/js/main.js
+++ uzduotys/diena10/giedriusgruz/1-js-projektas/js/main.js
@@ -1,17 +1,9 @@
 console.log("Labas");
 
-//  3 UZDUOTIS
-// paleisti f-ja printX 12 kartu ir atspausdinti <img ...
-var img = '<img src="" width="100px" height="100px">';
-for (var i = 0; i < 12; i++){
-    console.log("Martynas");
-    document.querySelector('.container').innerHTML += img;
-}
-var x = document.querySelector('.container');
-console.log(x);
-// 4 UZDUOTIS
-// sukurti f-jas piestiEilute(x); spausdintiStulpeli(x);  i konsole
-var tekstas = document.querySelector('h1').innerHTML;
-console.log("paimtas tekstas yra:", tekstas);
+// 
+var sarasas = [];
+for (var i = 0; i < 50; i++) {
+    // sarasas.push (0);
 
-document.querySelector('h1').textContent ="Hacked";
+}
+console.log(sarasas);
