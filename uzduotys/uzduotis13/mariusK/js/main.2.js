45f13559a073f3952589394a1768e2ccb355a734
Marius_Kuzminskas <33681369+MariusKuzminskas@users.noreply.github.com>
2018-01-05 10:20:21 +0200

    viskas is mariaus

---
+++ uzduotys/uzduotis13/mariusK/js/main.2.js
@@ -0,0 +1,29 @@
+console.log("Labas");
+
+var asdasd = [ 52, 97 ];
+
+// $.each(x, function( index, value ) {
+//   console.log('Stalcius: '+ index + ": " + value );
+// });
+// arba obj
+var obj = {
+  "name": "Tomas",
+  "age": "21"
+};
+
+
+//----js---
+{/* <button onclick="x.forEach(myFunction)">Try it</button> */}
+var x = [ 52, 97, 9, 10, 15 ]; 
+function myFunction(item,index,arr) {
+   arr[index] = item + 1;
+   console.log( index, item);
+}
+
+
+
+
+
+
+
+
