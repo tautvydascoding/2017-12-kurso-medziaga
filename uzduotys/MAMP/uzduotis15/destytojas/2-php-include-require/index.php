37dabd83e84a2d54440933a89522758a2aa48888
grajauskas <e.grajauskas@gmail.com>
2018-01-09 13:59:02 +0200

    16.1

--- uzduotys/MAMP/uzduotis15/destytojas/2-php-include-require/index.php
+++ uzduotys/MAMP/uzduotis15/destytojas/2-php-include-require/index.php
@@ -1,8 +1,8 @@
 
         <?php
 
-            include('./header.php');
+            require_once('./header.php');
             include('./content.php');
-            include('./footer.php');
-            
+            require_once('./footer.php');
+
          ?>
