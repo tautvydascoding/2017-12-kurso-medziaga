aa1da5ebe49b2c317b311e4c704f8e64bed0797b
Marius_Kuzminskas <33681369+MariusKuzminskas@users.noreply.github.com>
2018-01-10 17:38:26 +0200

    uzduotys su php ir msql

---
+++ uzduotys/MAMP/uzduotis15/mariusk/2-php-include/footer.php
@@ -0,0 +1,19 @@
+        <footer class="row aukstis-100 bg-warning">
+           <div class="col-12">Header</div>
+       </footer>
+        
+        
+        <!-- end container -->
+         </div>
+
+
+
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+         <!-- !!! mano javaScript visada failo apacioje ir pats apatinis -->
+         <script src="./js/main.js">
+         
+
+         </script>
+    </body>
+</html>
---
+++ uzduotys/MAMP/uzduotis15/mariusk/2-php-include/index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once ('./header.php');
+    include_once ('./content.php');
+    require_once ('./footer.php');
+?>       
+        
